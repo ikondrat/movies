c78bfd31e204809cdd376f91c328863fbe40e53d
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2017-11-21 10:44:58 +0100

    Update docs

--- src/containers/AsyncFilm.js
+++ src/containers/AsyncFilm.js
@@ -3,7 +3,6 @@ import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 import { 
   fetchFilmIfNeeded,
-  toggleFilm,
   toggleOneFilm
 } from '../actions'
 import Film from '../components/Film'
